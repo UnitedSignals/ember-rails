846516e9e681e3323c28158a4f43044800f5ac95
Thilo-Alexander Ginkel <thilo@ginkel.com>
2014-07-02 18:09:45 +0200

    Do not use temp path

--- lib/ember_rails.rb
+++ lib/ember_rails.rb
@@ -23,16 +23,6 @@ module Ember
       initializer "ember_rails.setup_vendor", :after => "ember_rails.setup", :group => :all do |app|
         variant = app.config.ember.variant || (::Rails.env.production? ? :production : :development)
 
-        # Copy over the desired ember, ember-data, and handlebars bundled in
-        # ember-source, ember-data-source, and handlebars-source to a tmp folder.
-        tmp_path = app.root.join("tmp/ember-rails")
-        ext = variant == :production ? ".prod.js" : ".js"
-        FileUtils.mkdir_p(tmp_path)
-        FileUtils.cp(::Ember::Source.bundled_path_for("ember#{ext}"), tmp_path.join("ember.js"))
-        FileUtils.cp(::Ember::Data::Source.bundled_path_for("ember-data#{ext}"), tmp_path.join("ember-data.js"))
-        FileUtils.chmod(0664, [tmp_path.join("ember.js"), tmp_path.join("ember-data.js")])
-        app.assets.append_path(tmp_path)
-
         # Make the handlebars.js and handlebars.runtime.js bundled
         # in handlebars-source available.
         app.assets.append_path(File.expand_path('../', ::Handlebars::Source.bundled_path))
